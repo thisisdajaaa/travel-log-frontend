e80e48ea445bd8b54829b104b695d13083aca98c
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2024-03-10 17:55:37 -0500

    feat: initial push

--- src/pages/travel-log/index.page.tsx
+++ src/pages/travel-log/index.page.tsx
@@ -1,7 +1,7 @@
 import { FormikContext, useFormik } from "formik";
 import { NextPage } from "next";
 import Image from "next/image";
-import React, { useCallback, useRef } from "react";
+import React, { useCallback, useMemo, useRef } from "react";
 import { FaX } from "react-icons/fa6";
 
 import { useFetchCountries } from "@/hooks";
@@ -11,10 +11,13 @@ import { Carousel, CarouselItem } from "@/components/Carousel";
 import FormDatePicker from "@/components/Formik/FormDatePicker";
 import FormImageUpload from "@/components/Formik/FormImageUpload";
 import FormInput from "@/components/Formik/FormInput";
+import FormSelect from "@/components/Formik/FormSelect";
 import FormTextArea from "@/components/Formik/FormTextArea";
 import { ImageUploadOptions } from "@/components/ImageUpload/config";
 import { FileWithPreview } from "@/components/ImageUpload/types";
 import Modal from "@/components/Modal";
+
+import type { Option } from "@/types/client";
 
 import { initialTravelLogForm } from "./fixtures";
 import type { TravelLogForm } from "./types";
@@ -33,6 +36,15 @@ const TravelLog: NextPage = () => {
     onSubmit: handleSubmit,
   });
 
+  const mappedCountryList: Option[] = useMemo(
+    () =>
+      countries?.data?.map((country) => ({
+        label: country.name,
+        value: country.id.toString(),
+      })) as Option[],
+    [countries]
+  );
+
   const handleShow = useCallback(() => {
     travelLogModalRef.current?.showModal();
   }, [travelLogModalRef]);
@@ -45,6 +57,8 @@ const TravelLog: NextPage = () => {
 
     formikBag.setFieldValue("files", newFiles);
   };
+
+  console.log("countries: ", countries);
 
   return (
     <div className="mx-auto my-10 max-w-4xl overflow-hidden rounded-lg">
@@ -78,6 +92,13 @@ const TravelLog: NextPage = () => {
 
             <FormDatePicker name="visitDate" isRequired label="Visit Date" />
 
+            <FormSelect
+              name="country"
+              label="Country"
+              options={mappedCountryList}
+              isRequired
+            />
+
             <div className="form-control w-full">
               <label className="label">
                 <span className="label-text font-bold">
@@ -91,45 +112,51 @@ const TravelLog: NextPage = () => {
               />
             </div>
 
-            <div className="grid grid-cols-2 gap-4">
-              <div className="h-[500px] w-[500px]">
-                <h4 className="text-md mb-3 font-bold uppercase">Preview</h4>
-                <Carousel display="sequential" className="rounded-box">
-                  {formikBag.values.files.map((file, index) => (
-                    <CarouselItem
-                      key={index}
-                      src={file?.preview}
-                      alt={file?.name}
-                    />
-                  ))}
-                </Carousel>
-              </div>
-
-              <div className="h-[500px] overflow-y-auto">
-                <h4 className="text-md font-bold uppercase">List</h4>
-                <ul className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
-                  {formikBag.values.files.map((file) => (
-                    <li key={file.name} className="group relative">
-                      <div className="aspect-w-16 aspect-h-9 image-blur-overlay h-56 w-full overflow-hidden rounded-md bg-gray-200">
-                        <Image
-                          src={file.preview}
-                          alt={`Thumbnail for ${file.name}`}
-                          layout="fill"
-                          objectFit="contain"
-                          className="image-content transition-transform duration-300 group-hover:scale-110"
+            {formikBag.values.files && formikBag.values.files.length > 0 && (
+              <div>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="h-[500px] w-[500px]">
+                    <h4 className="text-md mb-3 font-bold uppercase">
+                      Preview
+                    </h4>
+                    <Carousel display="sequential" className="rounded-box">
+                      {formikBag.values.files.map((file, index) => (
+                        <CarouselItem
+                          key={index}
+                          src={file?.preview}
+                          alt={file?.name}
                         />
-                      </div>
-                      <button
-                        className="absolute right-2 top-2 z-10 rounded-full bg-red-500 p-1 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-                        onClick={() => removeFile(file)}
-                      >
-                        <FaX size={10} />
-                      </button>
-                    </li>
-                  ))}
-                </ul>
+                      ))}
+                    </Carousel>
+                  </div>
+
+                  <div className="h-[500px] overflow-y-auto">
+                    <h4 className="text-md font-bold uppercase">List</h4>
+                    <ul className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
+                      {formikBag.values.files.map((file) => (
+                        <li key={file.name} className="group relative">
+                          <div className="aspect-w-16 aspect-h-9 image-blur-overlay h-56 w-full overflow-hidden rounded-md bg-gray-200">
+                            <Image
+                              src={file.preview}
+                              alt={`Thumbnail for ${file.name}`}
+                              layout="fill"
+                              objectFit="contain"
+                              className="image-content transition-transform duration-300 group-hover:scale-110"
+                            />
+                          </div>
+                          <button
+                            className="absolute right-2 top-2 z-10 rounded-full bg-red-500 p-1 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+                            onClick={() => removeFile(file)}
+                          >
+                            <FaX size={10} />
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </FormikContext.Provider>
       </Modal>
